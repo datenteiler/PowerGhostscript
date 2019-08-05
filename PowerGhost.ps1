baeb72055d84707d4d0e2af2b333cdb002db819f
Christian Imhorst <christian.imhorst@gmail.com>
2019-08-05 14:59:00 +0200

    Update PowerGhost.ps1

--- PowerGhost.ps1
+++ PowerGhost.ps1
@@ -9,51 +9,71 @@ function Convert-PDFtoTiff
    Convert-PDFtoTiff -pdf "C:\Users\Public\Franz.pdf" -tiff "C:\Users\Public\Franz.tiff" -dpi 600 -a4
 #>
 {
-    [CmdletBinding()]
-    [Alias()]
-    [OutputType([int])]
-    Param
-    (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string]$pdf,
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType([int])]
+  Param
+  (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$pdf,
 
-        [Parameter(Mandatory=$true, Position=1)]
-        [string]$tiff,
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]$tiff,
         
-        [Parameter(Mandatory=$true, Position=2)]
-        [int]$dpi,
+    [Parameter(Mandatory=$true, Position=2)]
+    [int]$dpi,
         
-        [Parameter(Mandatory = $true, ParameterSetName = 'byA4')]
-        [Switch]$a4,
+    [Parameter(Mandatory = $true, ParameterSetName = 'byA4')]
+    [Switch]$a4,
 
-        [Parameter(Mandatory = $true, ParameterSetName = 'byLetter')]
-        [Switch]$letter
-    )
+    [Parameter(Mandatory = $true, ParameterSetName = 'byLetter')]
+    [Switch]$letter
+  )
 
-    Begin
+  Begin
+  {
+    # Decode DLLs from text files
+    if (!(Test-Path $(Join-Path $PSScriptRoot "gsdll64.dll")))
+    {
+      certutil -decode gsdll64.txt gsdll64.dll | Write-Verbose
+    }
+    if (!(Test-Path $(Join-Path $PSScriptRoot "GhostscriptSharp.dll")))
+    {
+      certutil -decode GhostscriptSharp.txt GhostscriptSharp.dll | Write-Verbose
+    }
+    # Check if Ghostscript if in path
+    if (Test-Path $(Join-Path $PSScriptRoot "GhostscriptSharp.dll"))
     {
       Add-Type -Path $(Join-Path $PSScriptRoot "GhostscriptSharp.dll")
-      Add-Type -AssemblyName System.Drawing
     }
-    Process
+    else
     {
-      $Size = New-Object System.Drawing.Size($dpi,$dpi)
+      Write-Error "GhostscriptSharp.dll is missing!"
+    }
+    Add-Type -AssemblyName System.Drawing
+  }
+  Process
+  {
+    $Size = New-Object System.Drawing.Size($dpi,$dpi)
 
-      [string]$GsInputPath = $pdf
-      [string]$GsOutputPath = $tiff
-      $GsSettings = New-Object GhostscriptSharp.GhostscriptSettings
-      $GsSettings.Device = [GhostscriptSharp.Settings.GhostscriptDevices]::tiffpack
-      $GsSettings.Page.AllPages = $true
-      $GsSettings.Resolution = $Size
-      if ($a4) {
-        $GsSettings.Size.Native = [GhostscriptSharp.Settings.GhostscriptPageSizes]::a4
-      }
-      if ($letter) {
-        $GsSettings.Size.Native = [GhostscriptSharp.Settings.GhostscriptPageSizes]::letter
-      }
+    [string]$GsInputPath = $pdf
+    [string]$GsOutputPath = $tiff
+    $GsSettings = New-Object GhostscriptSharp.GhostscriptSettings
+    $GsSettings.Device = [GhostscriptSharp.Settings.GhostscriptDevices]::tiffpack
+    $GsSettings.Page.AllPages = $true
+    $GsSettings.Resolution = $Size
+    if ($a4) {
+      $GsSettings.Size.Native = [GhostscriptSharp.Settings.GhostscriptPageSizes]::a4
     }
-    End
-    {
-      [GhostscriptSharp.GhostscriptWrapper]::GenerateOutput($GsInputPath, $GsOutputPath, $GsSettings)
+    if ($letter) {
+      $GsSettings.Size.Native = [GhostscriptSharp.Settings.GhostscriptPageSizes]::letter
     }
+    
+  }
+  End
+  {
+    Write-Verbose "Create TIFF file $GsOutputPath with $($Size.Height) dpi as $($GsSettings.Size.Native)"
+    [GhostscriptSharp.GhostscriptWrapper]::GenerateOutput($GsInputPath, $GsOutputPath, $GsSettings)
+    Write-Verbose "TIFF file $GsOutputPath from $GsInputPath created."
+  }
 }
